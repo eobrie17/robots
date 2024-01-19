a8187eea1b5892d0f2f961295aa364dd09364ef5
emmaobrien <eobrie17@uvm.edu>
2024-01-18 21:02:18 -0500

    Created a floor

--- simulate.py
+++ simulate.py
@@ -1,9 +1,20 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 #this disables the sidebars
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.setGravity(0,0,-9.8)
+
+planeId = p.loadURDF("plane.urdf")
+
+
+p.loadSDF("box.sdf")
 
 for i in range(0, 1000):
     p.stepSimulation()
