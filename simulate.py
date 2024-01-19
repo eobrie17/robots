b21f3172b0ada18a90ff5fcda8c001d7a194cc92
emmaobrien <eobrie17@uvm.edu>
2024-01-18 19:51:45 -0500

    Created generate.py

--- simulate.py
+++ simulate.py
@@ -2,6 +2,8 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+#this disables the sidebars
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 for i in range(0, 1000):
     p.stepSimulation()
