2d50743ac5f45dd76f9164b0263f1bc9f7bf1230
emmaobrien <eobrie17@uvm.edu>
2024-01-30 10:19:27 -0500

    Made 5x5 tower

--- generate.py
+++ generate.py
@@ -5,14 +5,34 @@ pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
-x = 0
-y = 0
-z = .5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-x2 = 1
-y2 = 0
-z2 = 1.5
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
+
+x=0
+y=0
+z=.5
+
+#columns
+for i in range(5):
+    #rows
+    for j in range(5):
+        #this creates a tower
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+            z += 1
+            length *= .9
+            width *= .9
+            height *= .9
+        x += 1
+        #reset tower dimensions
+        length = 1
+        width = 1
+        height = 1
+        z = .5
+    #reset rows
+    x=0
+    y += 1
+
+
 
 pyrosim.End()
+
 
