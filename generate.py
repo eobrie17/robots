bbb04c6e4242219c8c1bd8837a52e487009c61ff
emmaobrien <eobrie17@uvm.edu>
2024-02-05 15:01:50 -0500

    hook shape with joints

--- generate.py
+++ generate.py
@@ -13,13 +13,13 @@ def create_world():
 
 def create_robot():
     pyrosim.Start_URDF("body.urdf")
-    length = 1
-    width = 1
-    height = 1
-    x=0
-    y=0
-    z=.5
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,.5], size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link2", pos=[0,0,.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,.5,.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0,.5,0], size=[1,1,1])
     pyrosim.End()
 
 create_world()
